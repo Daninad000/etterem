4bce34a2ae84f997a76167ffa3e00c0207abc880
Daninad000 <daniel.szabo.000@gmail.com>
2022-01-17 18:04:19 +0100

    Add files via upload

---
+++ regisztracio.php
@@ -0,0 +1,46 @@
+
+
+
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/style.css">
+        <title>Burger Étterem</title>
+    </head>
+    <body class="container bg-dark">
+        <header>
+            <img src="images/hatter/DSC_0921-Copy-1002x666.jpg" alt="étterem-háttér">
+
+            <div class="jumbotron jumbotron-fluid bg-dark">
+                <div class="text-center text-light bg-dark p-5 display-4">Burger Étterem</div>
+            </div>
+        </header>
+
+            
+        <?php
+            include_once("navbar.php");
+        ?>
+
+        
+        
+
+
+
+        <footer class="page-footer text-center wow fadeIn">
+            <div class="py-5 bg-dark">
+                <span class="footer-copyright text-secondary center" id="copyright">© 2022 Copyright:</span>
+                <span class="text-secondary center">Burger Étterem</span>
+            </div>
+        </footer>
+
+
+    </body>
+</html>
+
+
+
